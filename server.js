da2b9e01c4b37cad3c83008eb4c9cfea58700a99
Gheorghe Stefan Mihai <stefan.mihai.ghe@gmail.com>
2021-07-28 21:16:05 +0300

    make it better

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ require('dotenv').config();
 
 const app = express();
 
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
 app.get('/',()=>{
-  resizeBy.send('welcome to my');
 })
 
 app.post('/api/forma', (req,res) => {
